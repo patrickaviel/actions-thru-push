9ae4452eec3b240a0a655e2ae6c2210ac45027ab
Patrick Aviel Peralta <mrpatrickperalta@gmail.com>
2021-06-08 13:27:23 +0800

    Added Test Cases

--- test/git.test.js
+++ test/git.test.js
@@ -5,28 +5,28 @@ const chai = require('chai');
 const expect = chai.expect;
 
 
-// describe("Testing GitCommand.status()", function(){
+describe("Testing GitCommand.status()", function(){
 
-//     it('Should return information if has changes in directory', function(){
-//         let wd = new WorkingDirectory();
-//         wd.addFile("index.html", "views", "<html>Hello</html>");
-//         wd.addFile("index.js", "assets/scripts", "alert('Hi!')");
+    it('Should return information if has changes in directory', function(){
+        let wd = new WorkingDirectory();
+        wd.addFile("index.html", "views", "<html>Hello</html>");
+        wd.addFile("index.js", "assets/scripts", "alert('Hi!')");
 
-//         let git = new GitCommand(wd);
+        let git = new GitCommand(wd);
 
-//         let output = git.status();
+        let output = git.status();
 
-//         expect(output).to.equal('You have 2 change/s.\nviews/index.html\nassets/scripts/index.js');
-//     });
+        expect(output).to.equal('You have 2 change/s.\nviews/index.html\nassets/scripts/index.js');
+    });
 
-//     it('Should return information if no changes in directory', function(){
-//         let wd = new WorkingDirectory();
-//         let git = new GitCommand(wd);
-//         let output = git.status();
+    it('Should return information if no changes in directory', function(){
+        let wd = new WorkingDirectory();
+        let git = new GitCommand(wd);
+        let output = git.status();
 
-//         expect(output).to.equal('You have 0 change/s.\n');
-//     });
-// })
+        expect(output).to.equal('You have 0 change/s.\n');
+    });
+})
 
 describe("Testing GitCommand.add()", function(){
 
